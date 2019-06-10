33f8ad4c6e697cc9238fb0c79f317e4afb239b20
Ziyi Xi <xiziyi@msu.edu>
2019-06-10 15:40:49 -0400

    modify for using

--- snr/cal_snr_mpi.py
+++ snr/cal_snr_mpi.py
@@ -57,9 +57,10 @@ def maincal(search_urls):
     cal_search_urls = np.array_split(search_urls, size)[rank]
     proc_result_list = []
     model = TauPyModel(model="ak135")
+    print(cal_search_urls)
     for cal_search_url in cal_search_urls:
         wave_urls = glob(join(cal_search_url, "*Z"))
-        print(wave_urls)
+        print(cal_search_urls, cal_search_url, wave_urls)
         for wave_url in wave_urls:
             fname = wave_url.split("/")[-1].split(".")
             try:
