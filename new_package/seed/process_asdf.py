a1a41291f950e0a59957459f10cacd82276398f5
Ziyi Xi <xiziyi@msu.edu>
2019-06-23 15:19:45 -0700

    add draft for processing script

--- new_package/seed/process_asdf.py
+++ new_package/seed/process_asdf.py
@@ -75,8 +75,8 @@ if __name__ == "__main__":
     @click.option('--min_periods', required=True, type=str, help="min periods in seconds, eg: 10,40")
     @click.option('--max_periods', required=True, type=str, help="max periods in seconds, eg: 120,120")
     @click.option('--asdf_filename', required=True, type=str, help="asdf raw data file name")
-    @click.option('--waveform_length', required=True, type=str, help="waveform length to cut (from event start time)")
-    @click.option('--sampling_rate', required=True, type=str, help="sampling rate in HZ")
+    @click.option('--waveform_length', required=True, type=float, help="waveform length to cut (from event start time)")
+    @click.option('--sampling_rate', required=True, type=int, help="sampling rate in HZ")
     @click.option('--output_directory', required=True, type=str, help="output directory")
     def main(min_periods, max_periods, asdf_filename, waveform_length, sampling_rate, output_directory):
         min_periods = [float(item) for item in min_periods.split(",")]
