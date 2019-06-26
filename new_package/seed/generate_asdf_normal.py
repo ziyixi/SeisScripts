80e2585e9f9290f7dc3c05f85ba9b4b095289d12
Ziyi Xi <xiziyi@msu.edu>
2019-06-25 17:11:57 -0700

    use log for asdf generator

--- new_package/seed/generate_asdf_normal.py
+++ new_package/seed/generate_asdf_normal.py
@@ -8,29 +8,31 @@ import obspy
 import tempfile
 from os.path import join
 import subprocess
+from loguru import logger
 
 
-def generate_asdf_for_single_event(seed_directory, cmt_path, output_path, with_mpi):
+def generate_asdf_for_single_event(seed_directory, cmt_path, output_path, with_mpi, logfile):
+    logger.add(logfile, format="{time} {level} {message}", level="INFO")
     # generate asdf file
     if(not with_mpi):
         ds = pyasdf.ASDFDataSet(output_path, compression="gzip-3")
-        print("not with mpi, use gzip-3")
+        logger.info("not with mpi, use gzip-3")
     else:
         ds = pyasdf.ASDFDataSet(output_path, compression=None)
-        print("will use mpi, no compression")
+        logger.info("will use mpi, no compression")
 
     # readin eventxml
     event_xml = obspy.read_events(cmt_path)
 
     # add event xml to ds
-    print(f"adding event_xml {cmt_path}")
+    logger.info(f"adding event_xml {cmt_path}")
     ds.add_quakeml(event_xml)
     event = ds.events[0]
 
     # readin waves
     files = glob(join(seed_directory, "*"))
     for i, filename in enumerate(files):
-        print(f"adding waves #{i} {filename}")
+        logger.info(f"adding waves #{i} {filename}")
 
         # waveform_stream = read_stream(filename)
         dirpath = tempfile.mkdtemp()
@@ -44,7 +46,7 @@ def generate_asdf_for_single_event(seed_directory, cmt_path, output_path, with_m
     # to store only one event in ds)
     station_xml = obspy.core.inventory.inventory.Inventory()
     for i, filename in enumerate(files):
-        print(f"adding stationxml #{i} {filename}")
+        logger.info(f"adding stationxml #{i} {filename}")
 
         # sp = Parser(filename)
         # sp.write_xseed(station_xml_file_path)
@@ -65,7 +67,7 @@ def generate_asdf_for_single_event(seed_directory, cmt_path, output_path, with_m
 
     ds.add_stationxml(station_xml)
     del ds
-    print(f"success in creating {output_path}")
+    logger.success(f"success in creating {output_path}")
 
 
 def read_stream(filename):
@@ -84,8 +86,9 @@ if __name__ == "__main__":
     @click.option('--cmt_path', required=True, type=str, help="the CMTSOLUTION file for this event")
     @click.option('--output_path', required=True, type=str, help="the output path for hdf5 file")
     @click.option('--with_mpi/--no-with_mpi', default=False, help="if this file will be used with mpi (compression or not)")
-    def main(seed_directory, cmt_path, output_path, with_mpi):
+    @click.option('--logfile', required=True, type=str, help="the log file path")
+    def main(seed_directory, cmt_path, output_path, with_mpi, logfile):
         generate_asdf_for_single_event(
-            seed_directory, cmt_path, output_path, with_mpi)
+            seed_directory, cmt_path, output_path, with_mpi, logfile)
 
     main()
