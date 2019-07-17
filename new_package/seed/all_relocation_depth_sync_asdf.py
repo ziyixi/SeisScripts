1ec76d1c86c1ea7edcfb5682e48bccdbc48455ab
Ziyi Xi <xiziyi@msu.edu>
2019-07-17 10:56:26 -0400

    try to fix bug in convert sync

--- new_package/seed/all_relocation_depth_sync_asdf.py
+++ new_package/seed/all_relocation_depth_sync_asdf.py
@@ -2,22 +2,19 @@
 This is the serial version.
 """
 import click
-from mpi4py import MPI
+# from mpi4py import MPI
 from glob import glob
 from os.path import join
 import numpy as np
 from generate_sync_asdf_specfem import convert_sync_to_asdf
+import multiprocessing
 # ! no print, we have to find a way to log the process
 
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-isroot = (rank == 0)
-
-
-def get_directories_to_handle(all_dirs):
-    return np.array_split(all_dirs, size)[rank]
+# comm = MPI.COMM_WORLD
+# rank = comm.Get_rank()
+# size = comm.Get_size()
+# isroot = (rank == 0)
 
 
 @click.command()
@@ -25,19 +22,33 @@ def get_directories_to_handle(all_dirs):
 @click.option('--out_dir', required=True, type=str, help="the asdf output directory")
 def main(base_dir, out_dir):
     all_dirs = glob(join(base_dir, "*", "*"))
-    dirs_to_handle = get_directories_to_handle(all_dirs)
-    for each_dir in dirs_to_handle:
-        print(f"[{rank}] start to handle {each_dir}")
+#     for each_dir in all_dirs:
+#         print(f"start to handle {each_dir}")
+#         split_path = each_dir.split("/")
+#         event = split_path[-2]
+#         depth = split_path[-1]
+#         output_path = join(out_dir, f"sync_{event}_{depth}_raw.h5")
+#         files_in_output = glob(join(out_dir, "*"))
+#         if(output_path in files_in_output):
+#             continue
+
+#         convert_sync_to_asdf(each_dir, output_path, True)
+#         print(f"finish handling {each_dir}")
+
+    def kernel(each_dir):
+        print(f"start to handle {each_dir}")
         split_path = each_dir.split("/")
         event = split_path[-2]
         depth = split_path[-1]
         output_path = join(out_dir, f"sync_{event}_{depth}_raw.h5")
         files_in_output = glob(join(out_dir, "*"))
         if(output_path in files_in_output):
-            continue
-
+            return
         convert_sync_to_asdf(each_dir, output_path, True)
-        print(f"[{rank}] finish handling {each_dir}")
+        print(f"finish handling {each_dir}")
+
+    with multiprocessing.Pool(processes=9) as pool:
+        pool.map(kernel, all_dirs)
 
 
 if __name__ == "__main__":
