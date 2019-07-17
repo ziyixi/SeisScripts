a7013a9bb04f14c65a49df4d5214e214e1ace9c2
Ziyi Xi <xiziyi@msu.edu>
2019-07-17 14:25:09 -0400

    add slurm script to process data

--- new_package/seed/slurm_process_data.sh
+++ new_package/seed/slurm_process_data.sh
@@ -9,6 +9,7 @@
 ########## Command Lines to Run ##########
 RAW_DIR=/mnt/research/seismolab2/japan_slab/data/asdf_for_validation
 PROCESSED_DIR=/mnt/research/seismolab2/japan_slab/processed_data/processed_for_first_iteration_validation
+SIMPLE_DIR=/mnt/research/seismolab2/japan_slab/relocation/for_first_iteration_validation/simplified
 min_periods=10,20,40
 max_periods=120,120,120
 waveform_length=2400
@@ -27,8 +28,14 @@ cd /mnt/home/xiziyi/script/SeisScripts/new_package/seed                  ### cha
 for filename in $RAW_DIR/*.h5; do 
     # run process
     srun -n 150 $PY process_asdf_cea.py --min_periods $min_periods --max_periods $max_periods --asdf_filename $filename  --waveform_length $waveform_length --sampling_rate $sampling_rate  --output_directory $PROCESSED_DIR --logfile $logfile --correct_cea --cea_correction_file cmpaz_segment.txt
-    # remove empty stations
-    
 done
- 
+
+# remove empty stations
+for filename in $PROCESSED_DIR/*.h5; do
+    thebasename=`basename $filename`
+    $PY simplify_asdf.py --asdf_file $filename --logfile $logfile --output_file $SIMPLE_DIR/$thebasename 
+done
+
+# 
+
 scontrol show job $SLURM_JOB_ID     ### write job information to output file
