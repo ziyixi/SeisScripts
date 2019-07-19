2afbe92f2e48302bac063672facbce4c866e096b
Ziyi Xi <xiziyi@msu.edu>
2019-07-19 10:44:01 -0400

    add check sync script

---
+++ new_package/seed/check_asdf_processable.py
@@ -0,0 +1,38 @@
+"""
+some sync asdf file may have problem, we should check it.
+"""
+from glob import glob
+import pyasdf
+import numpy as np
+import click
+from os.path import join
+
+
+def get_all_files(main_dir):
+    return glob(join(main_dir, "*h5"))
+
+
+def check(fname):
+    try:
+        ds = pyasdf.ASDFDataSet(fname)
+        for item in ds.waveforms.list():
+            maxvalue = np.max(ds.waveforms[item].sync[0].data)
+        return True
+    except:
+        return False
+
+
+@click.command()
+@click.option('--main_dir', required=True, type=str, help="working directory")
+def main(main_dir):
+    all_files = get_all_files(main_dir)
+    for item in all_files:
+        status = check(item)
+        if(status):
+            pass
+        else:
+            print(item)
+
+
+if __name__ == "__main__":
+    main()
